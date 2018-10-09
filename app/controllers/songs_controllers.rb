ee0c4fa16bf76620e631ddc49c8e597bfd20890c
Mary Jack <maryjackinthebox@gmail.com>
2018-10-09 19:24:40 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controllers.rb
@@ -0,0 +1,14 @@
+class SongsController <ApplicationController
+  
+  before_action :set_params, only: [:show, :update, :edit, :destroy]
+  
+  def index
+    @songs = Song.all
+  end
+  
+  def new 
+    Song.new
+  end
+  
+  def create
+    @song= Song.new
