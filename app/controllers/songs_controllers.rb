143be4725f9224a25308ef07510aba2fe826e307
Mary Jack <maryjackinthebox@gmail.com>
2018-10-09 19:25:26 +0000

    automatically backed up by learn

--- app/controllers/songs_controllers.rb
+++ app/controllers/songs_controllers.rb
@@ -12,4 +12,36 @@ class SongsController <ApplicationController
   
   def create
     @song= Song.new(song_params)
-    
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+  def edit
+  end
+  def update
+    @song.update(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+  def destroy
+    @song.destroy
+     redirect_to songs_url
+  end
+  private
+  def set_params
+    @song = Song.find(params[:id])
+  end
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+end
