f26e3e15f7299b551c16e3bd22c7521aec3a4646
Mary Jack <maryjackinthebox@gmail.com>
2018-10-09 19:44:17 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@
 ActiveRecord::Schema.define(version: 20181009193012) do
 
   create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "released"
+    t.integer  "release_year"
+    t.string   "artist_name"
+    t.string   "genre"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
